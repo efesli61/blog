d02be77f844eed41c28c34eb40465724786ed72d
efesli61 <ozgurdurmus347@gmail.com>
2022-08-02 22:00:10 +0300

    deneme

--- manage.py
+++ manage.py
@@ -17,6 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-# Django Udemy Eğitimi Başlangıç
+# Django Udemy Eğitimi Başlangıçççç
 if __name__ == '__main__':
     main()
